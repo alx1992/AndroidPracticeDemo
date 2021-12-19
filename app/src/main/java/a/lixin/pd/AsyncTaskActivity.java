a31b38ae2ed8bd70caab8c658f98f2dad35fb39a
alx1992 <alx1992@qq.com>
2021-12-19 20:44:33 +0800

    第二次提交。

--- app/src/main/java/a/lixin/pd/AsyncTaskActivity.java
+++ app/src/main/java/a/lixin/pd/AsyncTaskActivity.java
@@ -24,7 +24,7 @@ public class AsyncTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asynctask);
-        
+        //
         initView();
         
     }
